71aa9971419afcd68bebac5239fe86bb85389878
FoxRox87 <87144897+FoxRox87@users.noreply.github.com>
2021-07-25 15:32:07 +0200

    ouch

---
+++ Objects/SawBlade.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export var damage := 10
+
+
+func _on_OuchTimer_timeout():
+	for body in $OuchArea.get_overlapping_bodies():
+		if body.is_in_group("hitable"):
+			body.hit(damage)
